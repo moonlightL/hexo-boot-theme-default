8010314096dd0865454ace8ee7a39771630556bc
ligw <jx8996@163.com>
2020-11-13 15:52:36 +0800

    first commit

---
+++ source/js/about.js
@@ -0,0 +1,67 @@
+let GuestBookManager = (function ($) {
+    let GuestBookManager = {
+        init: function(nickname) {
+            let flag = false;
+            let $footer = $("#footer-copyright");
+            let top = parseInt($footer.offset().top);
+            let winHeight = $(window).height();
+            if (top > winHeight) {
+                $(window).scroll(function(e) {
+                    let scrollTop = $(this).scrollTop();
+                    if (!flag && (winHeight + scrollTop >= top)) {
+                        // 获取留言列表
+                        flag = true;
+                        GuestBookManager.initComment(nickname);
+                    }
+                });
+            } else {
+                GuestBookManager.initComment(nickname);
+            }
+        },
+        initComment: function (nickname) {
+            $("#comment-container").BeautyComment({
+                title: "留言",
+                subTitle: "最新留言",
+                bloggerName: nickname,
+                baseUrl: "/admin/assets/custom/",
+                listUrl: "/guestBookList.json",
+                sendUrl: "/auth/sendGuestBook.json",
+                wrapClass: "ml-content",
+                ajaxParams: {pageNum: 1, pageSize: 10},
+                listHandler: function (resp) {
+                    return {
+                        totalNum: resp.data.totalNum,
+                        commentList: resp.data.commentList,
+                        commentShowType: resp.data.commentShowType
+                    }
+                },
+                sendHandler: function (resp) {
+                    return {
+                        success: resp.success,
+                        message: resp.message
+                    };
+                }
+            });
+        }
+    };
+
+    function isMobile(){
+        if ( navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    return {
+        init: GuestBookManager.init
+    }
+
+})(jQuery);
---
+++ source/js/main.js
@@ -0,0 +1,77 @@
+;(function() {
+
+    // 返回顶部
+    let goBack = function() {
+      let toTop = $("#toTop");
+      $(window).scroll(function(e) {
+           let scrollTop = $(this).scrollTop();
+
+           if (scrollTop > 500) {
+              toTop.removeClass("to-hide");
+           } else {
+             if (!toTop.hasClass("to-hide")) {
+                  toTop.addClass("to-hide");
+             }
+           }
+      });
+      toTop.on("click",function() {
+          $('html, body').animate({
+              scrollTop: $('html').offset().top
+            }, 500);
+      });
+    };
+
+    // 搜索
+    let search = function () {
+    $("#real-time-search-container").RealTimeSearch({
+        searchId: "search-btn",
+        listUrl: "/postList.json"
+    });
+    };
+
+    // 气泡
+    let circleMagic = function() {
+        $('.image-content').circleMagic({
+            radius: 16,
+            density: .1,
+            color: 'rgba(255,255,255, .4)',
+            clearOffset: .3
+        });
+    };
+
+    // 滚动
+    let contentWayPoint = function () {
+        let i = 0;
+        $('.animate-box').waypoint(function (direction) {
+            if (direction === 'down' && !$(this.element).hasClass('animated')) {
+                i++;
+                $(this.element).addClass('item-animate');
+                setTimeout(function () {
+                    $('body .animate-box.item-animate').each(function (k) {
+                        let el = $(this);
+                        setTimeout(function () {
+                            let effect = el.data('animate-effect');
+                            if (effect) {
+                                el.addClass(effect + ' animated');
+                            } else {
+                                el.addClass('fadeInUp animated');
+                            }
+                            el.removeClass('item-animate');
+                        }, k * 200, 'easeInOutExpo');
+                    });
+                }, 100);
+            }
+        }, {
+            offset: '85%'
+        });
+    };
+
+    $(function() {
+        goBack();
+        search();
+        circleMagic();
+        contentWayPoint();
+  });
+
+})();
+
